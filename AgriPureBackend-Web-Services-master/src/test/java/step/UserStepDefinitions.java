fef3a6fcd052af89ff3fe74ea5baaf636df084fd
Leonardo <u202017075@upc.edu.pe>
2023-05-02 18:39:55 -0500

    UserStepDefinitions

--- AgriPureBackend-Web-Services-master/src/test/java/step/UserStepDefinitions.java
+++ AgriPureBackend-Web-Services-master/src/test/java/step/UserStepDefinitions.java
@@ -21,14 +21,15 @@ public class UserStepDefinitions {
     private int randomServerPort;
     private String endpointPath;
     private ResponseEntity<String> responseEntity;
+
     @Given("The Endpoint {string} is available")
     public void theEndpointIsAvailable(String endpointPath) {
         this.endpointPath = String.format(endpointPath, randomServerPort);
     }
 
     @When("A user request is sent with values {string}, {string}, {string}, {string}")
-    public void aUserRequestIsSentWithValuesTrue(String username, String email, String password, Boolean premium) {
-        User user = new User(0L, username, email, password, true);
+    public void aUserRequestIsSentWithValues(String username, String email, String password, Boolean premium) {
+        User user = new User(0L, username, email, password, premium);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
@@ -54,8 +55,53 @@ public class UserStepDefinitions {
     public void aUserSelectedIsSentWithIdValue(String id_user) {
         Map<String, String> params = new HashMap<String, String>();
         params.put("id", id_user);
-        User user = testRestTemplate.getForObject(endpointPath + "{/id}", User.class);
+        User user = testRestTemplate.getForObject(endpointPath + "/{id}", User.class, params);
         responseEntity = new ResponseEntity<>(user.toString(), HttpStatus.OK);
         System.out.println(user.toString());
     }
+
+    @When("A user request is sent with invalid email {string}")
+    public void aUserRequestIsSentWithInvalidEmail(String email) {
+        User user = new User(0L, "user", email, "password", false);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<User> request = new HttpEntity<>(user, headers);
+        responseEntity = testRestTemplate.postForEntity(endpointPath, request, String.class);
+    }
+
+    @Then("A user with error status {int} is received")
+    public void aUserWithErrorStatusIsReceived(int expectedStatusCode) {
+        int actualStatus = responseEntity.getStatusCodeValue();
+        assertThat(expectedStatusCode).isEqualTo(actualStatus);
+    }
+
+    @When("A user request is sent with empty fields")
+    public void aUserRequestIsSentWithEmptyFields() {
+        User user = new User(0L, "", "", "", false);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<User> request = new HttpEntity<>(user, headers);
+        responseEntity = testRestTemplate.postForEntity(endpointPath, request, String.class);
+    }
+
+    @When("A user request is sent with an existing email {string}")
+    public void aUserRequestIsSentWithExistingEmail(String email) {
+        User user = new User(0L, "user", email, "password", false);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<User> request = new HttpEntity<>(user, headers);
+        responseEntity = testRestTemplate.postForEntity(endpointPath, request, String.class);
+    }
+
+    @Then("A user already exists response with status {int} is received")
+    public void aUserAlreadyExistsResponseWithStatusIsReceived(int expectedStatusCode) {
+        int actualStatus = responseEntity.getStatusCodeValue();
+        assertThat(expectedStatusCode).isEqualTo(actualStatus);
+    }
+
 }
